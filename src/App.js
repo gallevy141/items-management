eeb36465b6ed66989e76d9a62293f46e5723fac3
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-06-29 16:34:48 +0100

    Add basic components structure and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'green', color: 'black' }}>
+      <h1>Items Management</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div style={{ border: '2px solid green', color: 'green' }}>Items</div>
+        <div style={{ border: '2px solid green', color: 'black' }}>Item Details</div>
+        <div style={{ border: '2px solid green', color: 'black' }}>Add New Item</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
